a23a6309e50d1bf484de402d447350e05e62bc51
Jasper <blum.jasper@student.hpi.de>
2019-11-21 07:39:07 +0100

    Add exception for schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_11_07_143429) do
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +23,5 @@ ActiveRecord::Schema.define(version: 2019_11_07_143429) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
