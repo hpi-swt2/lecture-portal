64666ba549068772dfb729951b0dad755706099e
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2019-11-21 16:48:42 +0100

    add question scaffold generated model

---
+++ db/migrate/20191121154527_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.text :author
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
