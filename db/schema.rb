7107bc4f94ab53a422fe7b7355d9a1bbdd05a30d
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2019-11-22 12:04:18 +0100

    Add poll as scaffold and option poll as model and wrote tests for both

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_143429) do
+ActiveRecord::Schema.define(version: 2019_11_22_102114) do
+
+  create_table "poll_options", force: :cascade do |t|
+    t.text "description"
+    t.integer "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_poll_options_on_poll_id"
+  end
+
+  create_table "polls", force: :cascade do |t|
+    t.string "title"
+    t.boolean "is_multiselect"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
