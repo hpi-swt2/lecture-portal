a5a79e84c2cfb262c660c90f4eaf34be9629a5fa
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2019-11-24 17:34:41 +0100

    Remov password validation in the user model, since devise checks it automatically

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence:true
   validates :is_student, inclusion: { in: [ true, false ] }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
