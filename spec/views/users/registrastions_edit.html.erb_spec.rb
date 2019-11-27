6ba3c0e6dbe021caa45ae31d9d9e8d93eaa34035
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2019-11-27 11:02:58 +0100

    Add failing test for Christoph to see over

---
+++ spec/views/users/registrastions_edit.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "users/edit", type: :view do
+  let(:valid_session) { {} }
+  before(:each) do
+    assign(:user, User.new(
+        email: "user@example.com",
+        password: "password" ,
+        password_confirmation: "password" ,
+        is_student: true
+    ))
+  end
+
+  it "renders user editation form" do
+    login_student
+    visit edit_user_registration_path
+    expect(page).to have_field('user[email]')
+    expect(page).to have_field('user[password]')
+    expect(page).to have_field('user[password_confirmation]')
+    expect(page).to have_field('user[current_password]')
+    expect(page).to have_field('user[is_student]')
+  end
+
+  def login_student
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user = FactoryBot.create(:user, :student)
+    sign_in(user, scope: :user)
+  end
+end
+
+
