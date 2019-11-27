1228b49e20cacaff3430d9282a64a19190a88b7f
Christoph Matthies <chrisma@users.noreply.github.com>
2019-11-27 17:19:35 +0100

    Refactor user edit page feature test to remove duplication

--- spec/features/user_edit_spec.rb
+++ spec/features/user_edit_spec.rb
@@ -18,19 +18,24 @@ describe "Editing the user attributes on the 'user edit' page", type: :feature d
   end
 
   context "while logged in as a student" do
-    scenario "does not show an error" do
-      student = FactoryBot.create(:user, :student)
-      sign_in student
+    before(:each) do
+      @student = FactoryBot.create(:user, :student)
+      sign_in @student
       visit edit_user_registration_path
+    end
+
+    scenario "does not show an error" do
       expect(page).to_not have_css('.alert-danger')
       expect(page).to have_current_path(edit_user_registration_path)
     end
     
-    scenario "shows previously set attributes" do
-      student = FactoryBot.create(:user, :student)
-      sign_in student
-      visit edit_user_registration_path
-      expect(find_field('user[email]').value).to eq student.email
+    scenario "shows previously set email in input field" do
+      expect(find_field('user[email]').value).to eq @student.email
+    end
+
+    scenario "shows inputs for setting the password" do
+      expect(page).to have_field('user[password]')
+      expect(page).to have_field('user[password_confirmation]')
     end
   end
 end
