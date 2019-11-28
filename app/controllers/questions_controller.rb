f66f3d481c5f137434282bbd160f7d3665c6f807
Vincent Xeno Rahn <vinni@akv.i24.cc>
2019-11-28 12:16:59 +0100

    Sort questions by creation date descending, remove unused question controller definitions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
 
   # GET /questions
@@ -8,32 +7,9 @@ class QuestionsController < ApplicationController
 
   # GET api/questions
   def apiIndex
-    questions = Question.all
+    questions = Question.order(created_at: :desc)
     render json: questions
   end
-
-  # GET /questions/1
-  def show
-    question = Question.find(params[:id])
-    render json: question
-  end
-
-  # GET /questions/new
-  def new
-    @question = Question.new
-  end
-
-  # GET /questions/1/edit
-  def edit
-  end
-
-  # POST /questions
-  def create
-    question = Question.new(question_params)
-    if question.save
-    end
-  end
-
 
   # POST /api/questions
   def apiCreate
@@ -47,28 +23,7 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questions/1
-  def update
-    if @question.update(question_params)
-      redirect_to @question, notice: 'Question was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /questions/1
-  def destroy
-    @question.destroy
-    redirect_to questions_url, notice: 'Question was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_question
-      @question = Question.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
     def question_params
       params.require(:question).permit(:author, :content)
     end
