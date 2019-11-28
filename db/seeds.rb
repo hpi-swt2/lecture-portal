17859706e13b29dfd04aef7b26ad46961f5febb2
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-28 11:41:15 +0100

    add a default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
     #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
     #   Character.create(name: 'Luke', movie: movies.first)
     Lecture.create(name: "swt", enrollment_key: "epic", status: "created")
+    User.create(email: "user@example.com", password: "1234567890", password_confirmation: "1234567890")
