9f711bfbd25315a0132b4adb7f3351d9a24fc6ae
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-11-29 17:54:06 +0100

    Corrected rubocop offenses.

--- spec/features/lectures/show_lecture_spec.rb
+++ spec/features/lectures/show_lecture_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 describe "The show lecture page", type: :feature do
   it "should have no end button if the lecture is not running" do
-      @lecture = FactoryBot.create(:lecture)
-      @lecture.update(is_running: false)
-      visit(lecture_path(@lecture))
-      expect(page).not_to have_selector("input[type=submit][value='End']")
+        @lecture = FactoryBot.create(:lecture)
+        @lecture.update(is_running: false)
+        visit(lecture_path(@lecture))
+        expect(page).not_to have_selector("input[type=submit][value='End']")
       end
 
 
   it "should have an end button if the lecture is running" do
-      @lecture = FactoryBot.create(:lecture)
-      visit(lecture_path(@lecture))
-      expect(page).to have_selector("input[type=submit][value='End']")
+        @lecture = FactoryBot.create(:lecture)
+        visit(lecture_path(@lecture))
+        expect(page).to have_selector("input[type=submit][value='End']")
       end
 
   it "should end the lecture if the end button is clicked" do
