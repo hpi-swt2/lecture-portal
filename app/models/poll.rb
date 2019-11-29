8d83c58094ac2c19bd83e30c4e74a6edc435e6f0
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-11-29 16:12:02 +0100

    remove destroy dependency
    so a lecture does not get destroyed when one of it's polls is destroyed

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,6 +1,6 @@
 class Poll < ApplicationRecord
   has_many :poll_options, dependent: :destroy
-  belongs_to :lecture, dependent: :destroy
+  belongs_to :lecture
   validates :title, presence: true
   validates :is_multiselect, inclusion: { in: [true, false] }
 end
