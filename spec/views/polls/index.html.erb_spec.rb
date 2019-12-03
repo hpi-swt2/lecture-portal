1a5669a94f05b70a2174980d31fe9d3ba8c6f861
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-12-03 16:42:02 +0100

    adjust view poll tests to new model structure

--- spec/views/polls/index.html.erb_spec.rb
+++ spec/views/polls/index.html.erb_spec.rb
@@ -2,14 +2,17 @@ require "rails_helper"
 
 RSpec.describe "polls/index", type: :view do
   before(:each) do
+    @lecture = FactoryBot.create(:lecture)
     assign(:polls, [
       Poll.create!(
         title: "Title",
-        is_multiselect: false
+        is_multiselect: false,
+        lecture_id: @lecture.id
       ),
       Poll.create!(
         title: "Title",
-        is_multiselect: false
+        is_multiselect: false,
+        lecture_id: @lecture.id
       )
     ])
   end
