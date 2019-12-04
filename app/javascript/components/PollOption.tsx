b5bee9e348e6bf9a901f57dff78ba3eb5bf3d2f7
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-12-04 09:11:55 +0100

    Add react component that renders poll options
    * The number of options can be selected by the user
    * The inital number is 2

--- app/javascript/components/PollOption.tsx
+++ app/javascript/components/PollOption.tsx
@@ -1,28 +1,50 @@
 import * as React from "react"
 
 
-interface IPollOptionProps {
-  numberOfOptions: React.ReactNode;
-}
-
 interface IPollOptionState {
+    numberOfOptions: React.ReactNode;
 }
 
-class PollOption extends React.Component <IPollOptionProps, IPollOptionState> {
-  render() {
-    return <React.Fragment>
+class PollOption extends React.Component <IPollOptionState> {
 
-      <div className="field">
-        <text>Number of Options </text>
-        <input name="number_of_options" type = "number" onBlur={this.onBlur.bind(this)} />
-      </div>
-    </React.Fragment>
-    ;
-  }
+    state = {
+        numberOfOptions: 2
+    };
 
-  onBlur(){
-    alert("i was changed")
-  }
+
+    // We chose blur as an event so that a mistyped input does not directly erase any of the currently existing options.
+    // So this component only updates the view when the input focus leaves the number field.
+    onBlur(evt) {
+        if (evt.currentTarget && evt.currentTarget.value) {
+            this.setState({numberOfOptions: evt.currentTarget.value});
+        }
+    }
+
+    renderOptions() {
+        const options = [];
+        for (let index = 1; index <= this.state.numberOfOptions; ++index) {
+            const currentOption = <React.Fragment>
+                <br/>
+                <text>{index}. Option</text>
+                <input name={`${index}_option`} type="text" key={`${index}`}/></React.Fragment>;
+            options.push(currentOption);
+        }
+        return options;
+    }
+
+    render() {
+        const allOptions = this.renderOptions();
+        return <React.Fragment>
+
+            <div className="field">
+                <text>Number of Options</text>
+                <input name="number_of_options" type="number" onBlur={(evt) => this.onBlur(evt)}/>
+                {allOptions}
+            </div>
+        </React.Fragment>
+            ;
+    }
+
 }
 
 export default PollOption
