b90ae40714a9c72144f6fdeaaf4976c4d9b5de4a
Julian B <jeriox@users.noreply.github.com>
2019-12-06 14:54:45 +0100

    Feature/#35 Add tab view (#60)
    * [#31] Add feedback model
    
    Updated Rubocop and changed tests to fit the lecture reference of a feedback
    
    * Add tab view
    
    Co-authored-by: jeriox <jeriox@users.noreply.github.com>
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * [#31] Add feedback model
    
    Updated Rubocop and changed tests to fit the lecture reference of a feedback
    
    * Add tab view
    
    Co-authored-by: jeriox <jeriox@users.noreply.github.com>
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * [#31] Add feedback model to git
    
    * [#31] Add feedback files to git
    
    * [#31] Add feedback create and show to lecture feedback tab
    
    * [#31] Add migration and robucop style guides
    
    * [#31] Fix test for lecture show
    
    The test validates the existence of the navbar tabs
    
    * [#31] Add correct test description
    
    * Add role checking to lectures view page, setting are hidden for student\n\n\nCo-authored-by: LucPrestin <40767277+LucPrestin@users.noreply.github.com>\nCo-authored-by: jeriox <jeriox@users.noreply.github.com>
    
    * Fix Merge
    
    * Add submitting feedback without page reload
    
    * [#31] Fixed tests for the lecture overview
    
    * [#31] Add test for feedback model and add factory for feedback
    
    * Add usefull tests for tabs
    
    Co-authored-by: LucPrestin <40767277+LucPrestin@users.noreply.github.com>
    
    * Remove duplicate line
    
    Co-authored-by: LucPrestin <40767277+LucPrestin@users.noreply.github.com>

--- spec/views/lectures/show.html.erb_spec.rb
+++ spec/views/lectures/show.html.erb_spec.rb
@@ -10,25 +10,33 @@ RSpec.describe "lectures/show", type: :view do
                                   status: "created",
                                   lecturer: FactoryBot.create(:user, :lecturer, email: "bp@hpi.de")
     ))
-    # @student = FactoryBot.create(:user, :student)
   end
 
-  it "renders navbar tabs without settings for student" do
+  it "renders navbar tabs" do
     @current_user = FactoryBot.create(:user, :student)
     render
     assert_select "a", "Dashboard"
     assert_select "a", "Feedback"
     assert_select "a", "Questions"
     assert_select "a", "Polls"
+    expect(rendered).to have_css("#dashboard-tab")
+    expect(rendered).to have_css("#feedback-tab")
+    expect(rendered).to have_css("#questions-tab")
+    expect(rendered).to have_css("#polls-tab")
   end
 
-  it "renders navbar tabs with settings for lecturer" do
+  it "renders settings tab for lecturer" do
     @current_user = FactoryBot.create(:user, :lecturer)
     render
-    assert_select "a", "Dashboard"
-    assert_select "a", "Feedback"
-    assert_select "a", "Questions"
-    assert_select "a", "Polls"
     assert_select "a", "Settings"
+    expect(rendered).to have_content("Settings")
+    expect(rendered).to have_css("#settings-tab")
+  end
+
+  it "renders no settings tab for student" do
+    @current_user = FactoryBot.create(:user, :student)
+    render
+    expect(rendered).not_to have_content("Settings")
+    expect(rendered).not_to have_css("#settings-tab")
   end
 end
