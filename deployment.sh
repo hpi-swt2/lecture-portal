565ad594f72aa8c0afe89ec01bfd2e1bc2a9d0e3
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-06 16:02:40 +0100

    Allowed cd to run from this branch for testing purposes.

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@ echo "Currently running a $TRAVIS_EVENT_TYPE"
 if [ "$TRAVIS_BRANCH" = "master" ]
 then 
 	export APP="hpi-lectureportal"; 
-elif [ "$TRAVIS_BRANCH" = "dev" ]
+elif [ "$TRAVIS_BRANCH" = "fix/cd" ]
 then 
 	export APP="hpi-lectureportal-dev"; 
 else
