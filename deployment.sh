88579304e14320bf6f11c8806ec89d736f21f6cb
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-06 18:26:26 +0100

    Change dev branch back to dev.

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@ echo "Currently running a $TRAVIS_EVENT_TYPE"
 if [ "$TRAVIS_BRANCH" = "master" ]
 then 
 	export APP="hpi-lectureportal"; 
-elif [ "$TRAVIS_BRANCH" = "fix/cd" ]
+elif [ "$TRAVIS_BRANCH" = "dev" ]
 then 
 	export APP="hpi-lectureportal-dev"; 
 else
