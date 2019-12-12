b19af6361aee4f352f51a604088c9a64e6c9a19e
fabianhe <fabian.heseding@web.de>
2019-12-12 11:27:19 +0100

    Refactor QuestionList Model

--- app/javascript/stores/QuestionsList.ts
+++ app/javascript/stores/QuestionsList.ts
@@ -1,51 +1,49 @@
-import {destroy, Instance, types} from "mobx-state-tree";
-import question, {QuestionModel} from "./Question";
+import { destroy, Instance, types } from "mobx-state-tree";
+import Question, { QuestionModel } from "./Question";
 
 export type QuestionsListModel = Instance<typeof QuestionsList>
 
+const sortQuestionsList = (questionsList) => {
+    return questionsList.slice().sort((a: QuestionModel, b: QuestionModel): number => {
+        return (
+            b.created_at.getTime() -
+            a.created_at.getTime()
+        );
+    });
+}
+
+const createQuestionFromData = (questionData) => {
+    return Question.create({
+        id: questionData.id,
+        content: questionData.content,
+        author_id: questionData.author_id,
+        created_at: new Date(questionData.created_at)
+    });
+}
+
 const QuestionsList = types
     .model({
-        list: types.optional(types.array(question), []),
+        list: types.optional(types.array(Question), []),
     })
     .actions(self => ({
         addQuestion(questionData) {
-            self.list.push(question.create({
-                id: questionData.id,
-                content: questionData.content,
-                author_id: questionData.author_id,
-                created_at: new Date(questionData.created_at)
-            }));
-            self.list = self.list.sort((a, b): number => {
-                return (
-                    b.created_at.getTime() -
-                    a.created_at.getTime()
-                );
-            });
+            self.list.push(createQuestionFromData(questionData));
+            self.list = sortQuestionsList(self.list);
         },
         setQuestionsList(questionsListData) {
             self.list.clear();
             questionsListData.forEach(questionData => {
-                self.list.push(question.create({
-                    id: questionData.id,
-                    content: questionData.content,
-                    author_id: questionData.author_id,
-                    created_at: new Date(questionData.created_at)
-                }));
+                self.list.push(createQuestionFromData(questionData));
             });
-            self.list = self.list.sort((a, b): number => {
-                return (
-                    b.created_at.getTime() -
-                    a.created_at.getTime()
-                );
-            });
+            self.list = sortQuestionsList(self.list)
         },
         resolveQuestionById(id) {
             let resolvedQuestion: QuestionModel;
             self.list.forEach(question => {
-                if(question.id == id)
+                if (question.id == id)
                     resolvedQuestion = question
             });
-            if(resolvedQuestion)
+            if (resolvedQuestion)
                 destroy(resolvedQuestion)
         }
     }));
