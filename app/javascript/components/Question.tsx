c7c453b71af9bcc4e9df4da4b1f1763e86ba70a0
Fabian Galandi <fabian.galandi@student.hpi.de>
2019-12-12 11:35:21 +0100

    Fixed resolved button styling for lecturer

--- app/javascript/components/Question.tsx
+++ app/javascript/components/Question.tsx
@@ -2,23 +2,39 @@ import React from "react";
 import { observer } from "mobx-react";
 import { QuestionModel } from "../stores/Question";
 import { resolveQuestionById } from "../utils/QuestionsUtils";
+import { RootStoreModel } from "../stores/QuestionsRootStore";
+import useInject from "../hooks/useInject";
+
+
+const mapStore = ({ is_student, current_question }: RootStoreModel) => ({
+    is_student,
+    current_question
+});
 
 type Props = {
     question: QuestionModel
 }
 
 const QuestionView: React.FunctionComponent<Props> = ({ question }) => {
+    const { is_student } = useInject(mapStore);
 
     const onClick = _ => {
         resolveQuestionById(question.id)
     };
+
+    const className = ["btn btn-secondary"];
+    if (is_student) {
+        className.push("btn-sm");
+    } else {
+        className.push("btn-lg");
+    }
 
     return (
         <li key={question.id}>
             <div className="questionContent">
                 {question.content}
             </div>
-            <button className="btn btn-secondary btn-sm" onClick={onClick}>
+            <button className={className.join(" ").trim()} onClick={onClick}>
                 mark as solved
             </button>
         </li>
