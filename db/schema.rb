808257bd379d10f32caf76d7e730a43a9ee96e22
Moritz <moritz.spranger@gmail.com>
2019-12-13 10:36:54 +0100

    [#86] Add course scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_144904) do
+ActiveRecord::Schema.define(version: 2019_12_13_092312) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "status", default: "open"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "feedbacks", force: :cascade do |t|
     t.text "content"
