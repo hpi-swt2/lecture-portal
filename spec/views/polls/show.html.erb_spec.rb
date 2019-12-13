cd8f326f7dd15748f92188ecc2aecff815ba3b7a
Lara Pfennigschmidt <lara.pfennigschmidt@student.hpi.de>
2019-12-13 12:23:04 +0100

    add implementation for showing votes plus failing test

--- spec/views/polls/show.html.erb_spec.rb
+++ spec/views/polls/show.html.erb_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "polls/show", type: :view do
   before(:each) do
   	login_lecturer
     @lecture = FactoryBot.create(:lecture)
+    @poll_option = FactoryBot.create(:poll_option)
     @poll = FactoryBot.create(:poll)
   end
 
@@ -13,9 +14,28 @@ RSpec.describe "polls/show", type: :view do
     expect(rendered).to match(/false/)
   end
 
-  it "shows votes for the poll" do
+  it "shows votes for an inactive poll" do
+  	@poll.is_active = false
  	visit lecture_poll_path(@lecture, @poll)
-  	expect(page).to have_text('Votes')
+ 	within 'table' do
+  	  expect(page).to have_text("Votes")
+  	end
   end
-  
+
+  it "does not show votes for an active poll" do
+  	@poll.is_active = true #apparently this is not set during the test, the page shows it is still false
+  	visit lecture_poll_path(@lecture, @poll)
+  	#save_and_open_page
+  	within 'table' do
+  	  expect(page).to have_no_text("Votes")
+  	end
+
+  end
+
+
+  def login_lecturer
+    user = FactoryBot.create(:user, :lecturer)
+    sign_in(user, scope: :user)
+  end
+
 end
