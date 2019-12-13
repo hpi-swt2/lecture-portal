d2e2d6a829136314e873214ddb9eda410a16c7bd
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2019-12-13 16:49:40 +0100

    set default poll in specs to be active

--- spec/factories/polls.rb
+++ spec/factories/polls.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :poll do
     title { "MyString" }
     is_multiselect { false }
+    is_active { true }
     lecture_id { FactoryBot.create(:lecture).id }
   end
 end
