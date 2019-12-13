2d6812ffb6fb8871cd4763403700d0d36179ca78
Jasper <blum.jasper@student.hpi.de>
2019-12-13 10:47:13 +0100

    Add name validation
    Co-authored-by: LucPrestin <40767277+LucPrestin@users.noreply.github.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..40 }
   enum status: { open: "open", closed: "closed" }
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @course = FactoryBot.build(:course)
+  end
+
+  it "is creatable using a Factory" do
+    expect(@course).to be_valid
+  end
+
+  it "is not valid without a name" do
+    @course.name = ""
+    expect(@course).not_to be_valid
+  end
+
+  # it "is not valid without a creator" do
+  #   @course.creator = nil
+  #   expect(@course).not_to be_valid
+  # end
+  #
+  # it "persists lecturer as creator" do
+  #   expect(@course.creator).to exist
+  # end
+
+  it "is open by default after creation" do
+    expect(@course.status).to eq "open"
+  end
 end
