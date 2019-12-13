486ed2c0b7effbd2b774e4879b3d113afca5fb69
Michael Büßemeyer <michael.buessemeyer@student.hpi.de>
2019-12-13 12:35:27 +0100

    fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_131227) do
+ActiveRecord::Schema.define(version: 2019_12_06_144904) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "student_id"
