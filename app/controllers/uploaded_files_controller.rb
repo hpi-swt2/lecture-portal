3b27be34bc04e2d8b1b67c4027026aa2177ee66f
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-13 18:30:21 +0100

    Remove all not required methods from uploaded_files_controller.

--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -1,23 +1,14 @@
 class UploadedFilesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_uploaded_file, only: [:show, :edit, :update, :destroy]
 
   # GET /uploaded_files
   def index
     @uploaded_files = UploadedFile.all
   end
 
-  # GET /uploaded_files/1
-  def show
-  end
-
   # GET /uploaded_files/new
   def new
     @uploaded_file = UploadedFile.new
-  end
-
-  # GET /uploaded_files/1/edit
-  def edit
   end
 
   # POST /uploaded_files
@@ -44,27 +35,8 @@ class UploadedFilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /uploaded_files/1
-  def update
-    if @uploaded_file.update(uploaded_file_params)
-      redirect_to @uploaded_file, notice: 'Uploaded file was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /uploaded_files/1
-  def destroy
-    @uploaded_file.destroy
-    redirect_to uploaded_files_url, notice: 'Uploaded file was successfully destroyed.'
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_uploaded_file
-      @uploaded_file = UploadedFile.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def uploaded_file_params
       params.require(:uploaded_file).permit(:content_type, :filename, :data)
