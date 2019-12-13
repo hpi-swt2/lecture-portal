1d827b7c7d5b6fee1828c215c885d9eb3f269ff3
swietheger <swietheger@universum.com>
2019-12-13 11:02:31 +0100

    Add server side sorting of questions according to role of user

--- app/javascript/stores/QuestionsList.ts
+++ app/javascript/stores/QuestionsList.ts
@@ -3,13 +3,20 @@ import Question, { QuestionModel } from "./Question";
 
 export type QuestionsListModel = Instance<typeof QuestionsList>
 
-const sortQuestionsList = (questionsList) => {
-    return questionsList.slice().sort((a: QuestionModel, b: QuestionModel): number => {
-        return (
-            b.created_at.getTime() -
-            a.created_at.getTime()
-        );
-    });
+const sortQuestionsList = (questionsList, sortByTime) => {
+    return questionsList.slice().sort(sortByTime ? timeSorting : upvoteSorting);
+};
+
+const timeSorting = (a: QuestionModel, b: QuestionModel): number => {
+    return (
+        b.created_at.getTime() -
+        a.created_at.getTime()
+    );
+};
+
+const upvoteSorting = (a: QuestionModel, b: QuestionModel): number => {
+    const upvoteDiff = b.upvotes - a.upvotes;
+    return upvoteDiff == 0 ? timeSorting(a, b) : upvoteDiff;
 };
 
 const createQuestionFromData = (questionData) => {
@@ -26,18 +33,19 @@ const createQuestionFromData = (questionData) => {
 const QuestionsList = types
     .model({
         list: types.optional(types.array(Question), []),
+        is_sorted_by_time: types.optional(types.boolean, false),
     })
     .actions(self => ({
         addQuestion(questionData) {
             self.list.push(createQuestionFromData(questionData));
-            self.list = sortQuestionsList(self.list);
+            self.list = sortQuestionsList(self.list, self.is_sorted_by_time);
         },
         setQuestionsList(questionsListData) {
             self.list.clear();
             questionsListData.forEach(questionData => {
                 self.list.push(createQuestionFromData(questionData));
             });
-            self.list = sortQuestionsList(self.list)
+            self.list = sortQuestionsList(self.list, self.is_sorted_by_time)
         },
         resolveQuestionById(id) {
             let resolvedQuestion: QuestionModel;
@@ -48,14 +56,16 @@ const QuestionsList = types
             if (resolvedQuestion)
                 destroy(resolvedQuestion)
         },
-        upvoteQuestionById(id) : QuestionModel {
+        upvoteQuestionById(id): QuestionModel {
             let upvoteQuestion: QuestionModel;
             self.list.forEach(question => {
                 if (question.id == id)
                     upvoteQuestion = question
             });
-            if (upvoteQuestion)
+            if (upvoteQuestion) {
                 upvoteQuestion.upvote();
+                !self.is_sorted_by_time && (self.list = sortQuestionsList(self.list, self.is_sorted_by_time))
+            }
             return upvoteQuestion;
         }
     }));
