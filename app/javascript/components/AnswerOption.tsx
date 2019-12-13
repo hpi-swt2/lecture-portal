c663beb83c8c72d93bc87c9ffa9c8b9ee2026954
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-12-13 11:52:44 +0100

    return answers

--- app/javascript/components/AnswerOption.tsx
+++ app/javascript/components/AnswerOption.tsx
@@ -1,10 +1,15 @@
 import * as React from "react"
+import {ChangeEvent} from "react";
+import ts from "typescript/lib/tsserverlibrary";
+import nullCancellationToken = ts.server.nullCancellationToken;
 
 
 interface IAnswerOptionProps {
   options: Array<string>;
   title: string;
   is_multiselect: boolean;
+  lecture_id: number;
+  poll_id: number;
 }
 
 interface IAnswerOptionState {
@@ -13,8 +18,11 @@ interface IAnswerOptionState {
 }
 
 class AnswerOption extends React.Component <IAnswerOptionProps, IAnswerOptionState> {
+  answers = [];
   constructor(props) {
     super(props);
+    this.answers = new Array(props.options.length).fill(false);
+    console.log(this.answers);
     this.state = {numberOfOptions: props.options.length, options: props.options};
   }
 
@@ -22,36 +30,54 @@ class AnswerOption extends React.Component <IAnswerOptionProps, IAnswerOptionSta
     const allOptions = this.renderAnswers();
     return (
       <React.Fragment>
-        <h1>{this.props.title}</h1>
-        {allOptions}
+        <form onSubmit={this.submitAnswers}>
+          <h1>{this.props.title}</h1>
+          {allOptions}
 
-        <div className="actions">
-          <button type="submit">
-            <label>Save Answers</label>
-          </button>
-        </div>
+          <div className="actions">
+            <button type="submit" className="btn btn-primary">
+              <label>Save Answers</label>
+            </button>
+          </div>
+
+        </form>
       </React.Fragment>
     );
   }
 
   renderAnswers() {
-    let boxType;
-    if(this.props.is_multiselect){
-      boxType = "checkbox"
-    }else{
-      boxType = "radio"
-    }
-    const options = [];
-    for (let index = 1; index <= this.state.options.length; index++){
+    const {is_multiselect} = this.props;
+    const {options} = this.state;
+    const boxType = is_multiselect ? "checkbox" : "radio";
+    const optionElements = [];
+    for (let index = 1; index <= options.length; index++){
+      const option_name = is_multiselect ? `poll[option_${index}]` : "poll["
       const currentOption =
           <React.Fragment key={`frag_${index}`}>
             <br key={`br_${index}`}/>
-            <input id={`poll_option_${index}`} name={"poll[option]"} type={boxType} key={`${index}`}/>
-            <label key={`option_${index}_label`}> {index}. {this.state.options[index]} {" "} </label>
+            <input id={`poll_option_${index}`} name={"poll[option]"} type={boxType} key={`${index}`} onChange={(evt: ChangeEvent<HTMLInputElement>) => this.handleOptionChange(index,evt.target.checked)}/>
+            <label key={`option_${index}_label`}> {index}. {options[index]} {" "} </label>
           </React.Fragment>;
-      options.push(currentOption);
+      optionElements.push(currentOption);
     }
-    return options;
+    return optionElements;
+  }
+
+  // Use anonymous methods so it is automatically bound to this.
+  submitAnswers = (event) =>{
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const {lecture_id, poll_id} = this.props;
+    data.set("lecture_id", `${lecture_id}`);
+    data.set("id", `${poll_id}`);
+    data.set("answers", `${this.answers}`);
+    const csrf_token: HTMLMetaElement = document.head.querySelector("[name=csrf-token]");
+    const headers = new Headers({'C-CSRF-Token': csrf_token.content});
+    fetch(`/lectures/${lecture_id}/polls/${poll_id}/save_answers`, {method: "PUT", headers , body: data})
+  }
+
+  handleOptionChange = (option_id: number, is_selected: boolean) =>{
+    this.answers[option_id-1] = is_selected;
   }
 }
 
