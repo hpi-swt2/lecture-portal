f676908560be6c62277c3f3f80f2abf54f5da810
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2019-12-13 17:57:08 +0100

    Validate lecture to false if changed when the lecture is achived

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -8,6 +8,7 @@ class Lecture < ApplicationRecord
   validates :name, presence: true, length: { in: 2..40 }
   validates :enrollment_key, presence: true, length: { in: 3..20 }
   scope :active, -> { where status: "running" }
+  validates_with LectureValidator
 
 
   def set_active
@@ -29,4 +30,26 @@ class Lecture < ApplicationRecord
       self.participating_students.delete(student)
     end
   end
+
+  def ==(other_lecture)
+    return status == other_lecture.status && polls_enabled == other_lecture.polls_enabled && questions_enabled == other_lecture.questions_enabled \
+    && description == other_lecture.description && enrollment_key == other_lecture.enrollment_key && id == other_lecture.id
+  end
+
+  def !=(other_lecture)
+    return !(self == other_lecture)
+  end
 end
+
+class LectureValidator < ActiveModel::Validator
+  def validate(lecture)
+    if lecture.ended?
+      db_lecture = Lecture.find_by_lecturer_id(lecture.id)
+      attributes_changed = lecture != db_lecture
+      if attributes_changed
+        lecture.errors[:base] << "You cannot edit a lecture when is has been archived."
+      end
+    end
+  end
+end
+
