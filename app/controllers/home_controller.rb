1bece932424b8354b61235a9baddee897cef8db4
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2019-12-14 13:16:45 +0100

    [#95] Add list of all courses from a lecturer to the homeview

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @current_user = current_user
+    @courses = Course.where(creator: current_user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_095526) do
+ActiveRecord::Schema.define(version: 2019_12_13_135304) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
