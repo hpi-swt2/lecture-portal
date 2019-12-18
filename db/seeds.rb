fd08847c896508804bb39ed1a50a27cecbf1d741
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-12-18 16:04:36 +0100

    add some seed users & lectures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
     # This file should contain all the record creation needed to seed the database with its default values.
     # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-    #
-    # Examples:
-    #
-    #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-    #   Character.create(name: 'Luke', movie: movies.first)
-    Lecture.create(name: "swt", enrollment_key: "epic", status: "created")
-    User.create(email: "user@example.com", password: "1234567890", password_confirmation: "1234567890")
+
+    lecturer_1 = User.create(is_student: false, email: "lecturer-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
+    student_1 = User.create(is_student: true, email: "student-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
+
+    Lecture.create(name: "<Created Lecture>", enrollment_key: "epic", status: "created", lecturer: lecturer_1)
+    Lecture.create(name: "<Running Lecture>", enrollment_key: "epic", status: "running", lecturer: lecturer_1)
+    Lecture.create(name: "<Ended Lecture>", enrollment_key: "epic", status: "ended", lecturer: lecturer_1)
