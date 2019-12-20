4a50d4dc64a0a72099143aaa224f2d9f63dde3f5
Eric Ackermann <eric.ackermann@student.hpi.de>
2019-12-20 13:22:51 +0100

    Show Type in category.

--- spec/features/all_uploaded_files_spec.rb
+++ spec/features/all_uploaded_files_spec.rb
@@ -9,6 +9,7 @@ describe "Show uploaded files index page", type: :feature do
     @lecturer = FactoryBot.create(:user, :lecturer)
     @lecture = FactoryBot.create(:lecture, lecturer: @lecturer)
     file = UploadedFile.new(filename: "LICENSE", content_type: "text/plain", data: data, allowsUpload: @lecture)
+    file.Notes!
     file.save
     sign_in @lecturer
   end
@@ -17,13 +18,27 @@ describe "Show uploaded files index page", type: :feature do
     visit uploaded_files_path
     # setup worked, else rest of test is pointless
     expect(UploadedFile.all.size).to eq(1)
-    expect(find(:table_row, { "Filename" => "LICENSE" }, {})).to have_text("text/plain")
+    expect(find(:table_row, {"Filename" => "LICENSE" }, {})).to have_text("text/plain")
   end
 
   it "should show the license text" do
     visit uploaded_files_path
     # setup worked, else rest of test is pointless
     expect(UploadedFile.all.size).to eq(1)
-    expect(find(:table_row, { "Filename" => "LICENSE" }, {})).to have_text("WITHOUT WARRANTY OF ANY KIND")
+    expect(find(:table_row, {"Filename" => "LICENSE" }, {})).to have_text("WITHOUT WARRANTY OF ANY KIND")
+    end
+
+  it "should show the license text" do
+    visit uploaded_files_path
+    # setup worked, else rest of test is pointless
+    expect(UploadedFile.all.size).to eq(1)
+    expect(find(:table_row, {"Filename" => "LICENSE" }, {})).to have_text("WITHOUT WARRANTY OF ANY KIND")
+    end
+
+  it "should show the \"Notes\" category" do
+    visit uploaded_files_path
+    # setup worked, else rest of test is pointless
+    expect(UploadedFile.all.size).to eq(1)
+    expect(find(:table_row, {"Filename" => "LICENSE" }, {})).to have_text "Notes"
   end
 end
