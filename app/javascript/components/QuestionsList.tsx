41a3c0c147c087e34e6c73465998a13dc0e148ea
swietheger <swietheger@universum.com>
2019-12-20 14:16:56 +0100

    Remove top margin questions list lecturers view

--- app/javascript/components/QuestionsList.tsx
+++ app/javascript/components/QuestionsList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { observer } from "mobx-react";
 import Question from "./Question";
 import { QuestionsRootStoreModel } from "../stores/QuestionsRootStore";
-import useInject from "../hooks/useInject"
+import useInject from "../hooks/useInject";
 
 const mapStore = ({ is_student, questionsList }: QuestionsRootStoreModel) => ({
   is_student,
@@ -13,17 +13,17 @@ const QuestionsList: React.FunctionComponent<{}> = observer(() => {
   const { is_student, questionsList } = useInject(mapStore);
 
   const onSortingClick = e => {
-    questionsList.toggleSorting()
-  }
+    questionsList.toggleSorting();
+  };
 
   return (
-    <div className="questionsList">
+    <div className={"questionsList" + (is_student ? "" : " is_lecturer")}>
       <div className="questionsFilter">
         <div className="sorting" onClick={onSortingClick}>
           {questionsList.is_sorted_by_time ? "time" : "votes"}
         </div>
       </div>
-      <ul className={(is_student ? "" : "is_lecturer")}>
+      <ul className={is_student ? "" : "is_lecturer"}>
         {questionsList.list.map(question => (
           <Question question={question} key={question.id} />
         ))}
