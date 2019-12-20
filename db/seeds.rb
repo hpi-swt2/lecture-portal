104f2c30d115246cf74fae9468717e97aa0ca944
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-12-20 14:57:11 +0100

    remove unused var

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
     # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
     lecturer_1 = User.create(is_student: false, email: "lecturer-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
-    student_1 = User.create(is_student: true, email: "student-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
+    User.create(is_student: true, email: "student-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
 
     Lecture.create(name: "<Created Lecture>", enrollment_key: "epic", status: "created", lecturer: lecturer_1)
     Lecture.create(name: "<Running Lecture>", enrollment_key: "epic", status: "running", lecturer: lecturer_1)
