6ed8b6dc7f9a0f918b946a19737ca4c9c0274282
m0e33 <moritz.spranger@gmail.com>
2019-12-21 17:15:36 +0100

    [#100] Add join button for course in home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,13 @@ class HomeController < ApplicationController
     @courses = Course.where(creator: current_user)
     @open_courses = Course.where(status: "open")
   end
+
+  def join_course
+    @course = Course.find(params[:id])
+    @course.join_course(current_user)
+    @course.save
+    current_user.save
+    redirect_to @course, notice: "You successfully joined the lecture."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/lectures/current", to: "lectures#current", as: "current_lectures"
   post "/lectures/start_lecture", to: "lectures#start_lecture", as: "start_lecture"
   post "/lectures/join_lecture", to: "lectures#join_lecture", as: "join_lecture"
-  post "/courses/join_course", to: "courses#join_course", as: "join_course"
+  post "/home/join_course", to: "home#join_course", as: "join_course"
   post "/lectures/end_lecture", to: "lectures#end_lecture", as: "end_lecture"
 
   resources :lectures do
