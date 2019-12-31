1e378d96e4e308a54af87ed75c30212ff852e54a
Gritta Weisheit <gritta.weisheit@giz.berlin>
2019-12-31 01:19:23 +0100

    try changing a migation so build does not fail

--- db/migrate/20191129151616_add_is_active_to_poll.rb
+++ db/migrate/20191129151616_add_is_active_to_poll.rb
@@ -1,5 +1,5 @@
 class AddIsActiveToPoll < ActiveRecord::Migration[5.2]
   def change
-    change_column :polls, :is_active, :boolean, null: false, default: true
+    add_column :polls, :is_active, :boolean, null: false, default: true
   end
 end
