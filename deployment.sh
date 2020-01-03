61bb2ec0110a13773ea3a841b923fefb39e0e037
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-01-03 18:00:45 +0100

    Run deployment from dev and master.

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@ echo "Currently running a $TRAVIS_EVENT_TYPE"
 if [ "$TRAVIS_BRANCH" = "master" ]
 then 
 	export APP="hpi-lectureportal"; 
-elif [ "$TRAVIS_BRANCH" = "ab-sassc-platform-independent" ]
+elif [ "$TRAVIS_BRANCH" = "dev" ]
 then 
 	export APP="hpi-lectureportal-dev"; 
 else
