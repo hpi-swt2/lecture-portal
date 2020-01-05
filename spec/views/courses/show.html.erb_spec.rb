8c4290ea6a7850d1efc3ec211b422328f18b1bb7
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-05 17:51:38 +0100

    [#94] Refactor tests for lectures views

--- spec/views/courses/show.html.erb_spec.rb
+++ spec/views/courses/show.html.erb_spec.rb
@@ -3,12 +3,21 @@ require "rails_helper"
 RSpec.describe "courses/show", type: :view do
   before(:each) do
     @course = FactoryBot.create(:course)
+    @lecture = FactoryBot.create(:lecture, name: "Name", enrollment_key: "Enrollment", status: "created",  course: @course)
+    @current_user = FactoryBot.create(:user, :lecturer)
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes of the course in <p>" do
     render
     expect(rendered).to match(/SWT2/)
     expect(rendered).to match(/ruby/)
     expect(rendered).to match(/open/)
+  end
+
+  it "renders a list of lectures" do
+    render
+    assert_select "tr>td", text: "Name".to_s, count: 1
+    assert_select "tr>td", text: "Enrollment".to_s, count: 1
+    assert_select "tr>td", text: "created".to_s, count: 1
   end
 end
--- spec/views/lectures/index.html.erb_spec.rb
+++ spec/views/lectures/index.html.erb_spec.rb
@@ -2,26 +2,32 @@ require "rails_helper"
 
 RSpec.describe "lectures/index", type: :view do
   before(:each) do
+    @course = FactoryBot.create(:course)
     assign(:lectures, [
       Lecture.create!(
         name: "Name",
         enrollment_key: "Enrollment Key",
         status: "created",
-        lecturer: FactoryBot.create(:user, :lecturer, email: "hp@hpi.de")
+        lecturer: FactoryBot.create(:user, :lecturer, email: "hp@hpi.de"),
+        course:@course
       ),
       Lecture.create!(
         name: "Name",
         enrollment_key: "Enrollment Key",
         status: "created",
-        lecturer: FactoryBot.create(:user, :lecturer, email: "cm@hpi.de")
+        lecturer: FactoryBot.create(:user, :lecturer, email: "cm@hpi.de"),
+        course:@course
       )
     ])
   end
 
+# this test is now in courses/show.html.erb_spec due that lectures don't have an own index anymore
+=begin
   it "renders a list of lectures" do
     render
     assert_select "tr>td", text: "Name".to_s, count: 2
     assert_select "tr>td", text: "Enrollment Key".to_s, count: 2
     assert_select "tr>td", text: "created".to_s, count: 2
   end
+=end
 end
