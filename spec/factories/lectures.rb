163391a94dc080b3028d08540779367bb0b15664
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-05 10:15:19 +0100

    [#100] Add factory for lectures

--- spec/factories/lectures.rb
+++ spec/factories/lectures.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :lecture do
-    name { "SWTII" }
+    name { "SWT2" }
     enrollment_key { "ruby" }
     lecturer { FactoryBot.create(:user, :lecturer) }
+    course { FactoryBot.create(:course)}
   end
 end
