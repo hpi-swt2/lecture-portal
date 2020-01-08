5b45603662a42a7c0031caf789d664c1f324632a
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2020-01-08 14:00:11 +0100

    Delete unnecessary files for react database structure

--- app/javascript/hooks/useInject.ts
+++ app/javascript/hooks/useInject.ts
@@ -3,9 +3,7 @@ import {UpdatesRootStoreModel} from "../stores/UpdatesRootStore";
 import {useQuestionsStore} from "../utils/QuestionsUtils";
 import {useUpdatesStore} from "../utils/UpdatesUtils";
 import { PollOptionsRootStoreModel } from "../stores/PollOptionsRootStore"
-import {UpdatePollOptionsRootStoreModel} from "../stores/UpdatePollOptionsRootStore";
 import {usePollOptionsStore} from "../utils/PollOptionUtils";
-import {useUpdatePollOptionsStore} from "../utils/PollOptionUpdatesUtils";
 
 export type QuestionsMapStore<T> = (store: QuestionsRootStoreModel) => T
 export type UpdatesMapStore<T> = (store: UpdatesRootStoreModel) => T
@@ -20,13 +18,8 @@ export const useInjectUpdates = <T>(mapStore: UpdatesMapStore<T>) => {
 };
 
 export type PollOptionsMapStore<T> = (store: PollOptionsRootStoreModel) => T
-export type PollOptionsUpdatesMapStore<T> = (store: UpdatePollOptionsRootStoreModel) => T
 
 export const useInjectPollOptions = <T>(mapStore: PollOptionsMapStore<T>) => {
     const store = usePollOptionsStore();
-    return mapStore(store)
-};
-export const useInjectPollOptionsUpdates = <T>(mapStore: PollOptionsUpdatesMapStore<T>) => {
-    const store = useUpdatePollOptionsStore();
     return mapStore(store)
 };
