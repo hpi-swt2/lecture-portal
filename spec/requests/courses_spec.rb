a0402ee8e9ebaab2c1fcb5c44172144ff27b1b9f
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-09 17:00:44 +0100

    [#92] Fix tests

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -2,9 +2,10 @@ require "rails_helper"
 
 RSpec.describe "Courses", type: :request do
   describe "GET /courses" do
-    it "works! (now write some real specs)" do
+
+    it "redirects when not logged in" do
       get courses_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
