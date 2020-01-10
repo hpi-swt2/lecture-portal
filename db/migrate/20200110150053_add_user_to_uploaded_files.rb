5841bb70a67ed11daf75b30c697a7ac495f8eac5
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2020-01-10 17:41:17 +0100

    Add index for Author-UploadedFiles-Assosiation

--- db/migrate/20200110150053_add_user_to_uploaded_files.rb
+++ db/migrate/20200110150053_add_user_to_uploaded_files.rb
@@ -1,5 +1,5 @@
 class AddUserToUploadedFiles < ActiveRecord::Migration[5.2]
   def change
-    add_reference :uploaded_files, :author, foreign_key: { to_table: "users" }
+    add_reference :uploaded_files, :author, foreign_key: { to_table: "users" }, index: true
   end
 end
