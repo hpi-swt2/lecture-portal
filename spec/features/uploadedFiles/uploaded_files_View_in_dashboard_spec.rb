2179c0f3854e53d6cf9eb84bbeaee5fa5a55e610
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-01-10 18:21:41 +0100

    Add test that checks if only files of the current lecture are shown.

--- spec/features/uploadedFiles/uploaded_files_View_in_dashboard_spec.rb
+++ spec/features/uploadedFiles/uploaded_files_View_in_dashboard_spec.rb
@@ -7,17 +7,21 @@ describe "The lecture dashboard page", type: :feature do
   context "having uploaded two files" do
     before :each do
       @link_addr = "https://hpi.de"
-      @file = file_fixture("LICENSE")
+      @file_addr = file_fixture("LICENSE")
       @lecturer = FactoryBot.create(:user, :lecturer)
       @lecture = FactoryBot.create(:lecture, lecturer: @lecturer)
-      @lecture.set_active
-      @lecture.save
+      @other_lecture = FactoryBot.create(:lecture, lecturer: @lecturer)
+      @other_lecture
+      @other_lecture.save
       @course = @lecture.course
       @link = UploadedFile.new(data: @link_addr, allowsUpload: @lecture, isLink: true)
       @link.save
       @filename = "LICENSE"
-      @file = UploadedFile.new(filename: @filename, content_type: "text/plain", data: @file.read, allowsUpload: @lecture, isLink: false)
+      @filename2 = "OTHER LICENSE"
+      @file = UploadedFile.new(filename: @filename, content_type: "text/plain", data: @file_addr.read, allowsUpload: @lecture, isLink: false)
       @file.save
+      @file2 = UploadedFile.new(filename: @filename2, content_type: "text/plain", data: @file_addr.read, allowsUpload: @other_lecture, isLink: false)
+      @file2.save
       sign_in @lecturer
     end
 
@@ -29,6 +33,11 @@ describe "The lecture dashboard page", type: :feature do
     it "shows the file name" do
       visit course_lecture_path(@course, @lecture)
       expect(page).to have_text(@filename)
+      end
+
+    it "does not show the files of other lectures" do
+      visit course_lecture_path(@course, @lecture)
+      expect(page).to_not have_text(@filename2)
     end
 
     it "has the \"Download\" button" do
