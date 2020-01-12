dd1b8e2ed90c2db665756f8736e4f8ffc869dc83
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-01-12 10:47:28 +0100

    adapt seed data to have a course

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,10 @@
 
     lecturer_1 = User.create(is_student: false, email: "lecturer-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
     User.create(is_student: true, email: "student-1@hpi.de", password: "1234567890", password_confirmation: "1234567890")
+    User.create(is_student: true, email: "student-2@hpi.de", password: "1234567890", password_confirmation: "1234567890")
 
-    Lecture.create(name: "<Created Lecture>", enrollment_key: "epic", status: "created", lecturer: lecturer_1)
-    Lecture.create(name: "<Running Lecture>", enrollment_key: "epic", status: "running", lecturer: lecturer_1)
-    Lecture.create(name: "<Ended Lecture>", enrollment_key: "epic", status: "ended", lecturer: lecturer_1)
+    course_1 = Course.create(name: "<Kurs-1>", description: "Testkurs #1", creator: lecturer_1)
+
+    Lecture.create(name: "<Created Lecture>", enrollment_key: "epic", status: "created", lecturer: lecturer_1, course: course_1)
+    Lecture.create(name: "<Running Lecture>", enrollment_key: "epic", status: "running", lecturer: lecturer_1, course: course_1)
+    Lecture.create(name: "<Ended Lecture>", enrollment_key: "epic", status: "ended", lecturer: lecturer_1, course: course_1)
