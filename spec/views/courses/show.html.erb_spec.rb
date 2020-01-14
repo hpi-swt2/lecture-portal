516337cff91b1310a7fb2aaed73230467e72aacd
Christopher Aust <38725693+caustt@users.noreply.github.com>
2020-01-14 21:19:20 +0100

    Feature/#155 course file view (#189)
    * add prototype for filelist
    
    * add download link
    
    *  Tests for #155, remove double tests for show_courses
    
    * Differntiate student and lecturer files and add scroll bar
    
    * Missing test case
    
    * fix merge
    
    Co-authored-by: Adrian Jost <22987140+adrianjost@users.noreply.github.com>

--- spec/views/courses/show.html.erb_spec.rb
+++ spec/views/courses/show.html.erb_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe "courses/show", type: :view do
     @course = FactoryBot.create(:course, creator: @lecturer)
     @lecture = FactoryBot.create(:lecture, name: "Name", enrollment_key: "Enrollment", status: "created",  course: @course, lecturer: @lecturer)
     @current_user = FactoryBot.create(:user, :lecturer)
+    @student_files = []
+    @lecturer_files = []
     sign_in @lecturer
   end
 
   it "renders title and description of the course " do
     render
-    expect(rendered).to have_text("SWT2")
-    expect(rendered).to have_text("ruby")
+    expect(rendered).to match(@course.name)
+    expect(rendered).to match(@course.description)
   end
 
   it "renders a list of lectures" do
@@ -21,53 +23,5 @@ RSpec.describe "courses/show", type: :view do
     assert_select "tr>td", text: @lecture.name, count: 1
     assert_select "tr>td", text: @lecture.enrollment_key, count: 1
     assert_select "tr>td", text: @lecture.status, count: 1
-  end
-
-  it "should have a \"Start\" link for not started lecture" do
-    visit(course_path(id: @lecture.course.id))
-
-    expect(page).to have_link("Start", href: start_lecture_path(course_id: @lecture.course.id) + "?id=" + @lecture.id.to_s)
-  end
-
-  it "should not have a \"View\" link for not started lecture" do
-    visit(course_path(id: @lecture.course.id))
-    expect(page).to_not have_link("View", href: course_lecture_path(course_id: @lecture.course.id, id: @lecture.id))
-  end
-
-  it "should have a \"Create Lecture\" button" do
-    visit(course_path(id: @lecture.course.id))
-    expect(page).to have_link("Create Lecture")
-  end
-
-  it "should set the lecture active on clicking \"Start\"" do
-    visit(course_path(id: @lecture.course.id))
-    click_on("Start")
-    @lecture.reload
-    expect(@lecture.status).to eq("running")
-  end
-
-  it "should redirect to the show path after clicking \"Start\"" do
-    visit(course_path(id: @lecture.course.id))
-    click_on("Start")
-    expect(current_path).to eq(course_lecture_path(course_id: @lecture.course.id, id: @lecture.id))
-  end
-
-  it "should not show the \"Start\" button after a lecture was started" do
-    visit(course_path(id: @lecture.course.id))
-    click_on("Start")
-    expect(page).not_to have_selector("input[type=submit][value='Start']")
-  end
-
-  it "should show a \"View\" link after the lecture is started" do
-    @lecture.update(status: "running")
-    visit(course_path(id: @lecture.course.id))
-    expect(page).to have_link("View", href: course_lecture_path(course_id: @lecture.course.id, id: @lecture.id))
-  end
-
-  it "should not show lectures of other lecturers" do
-    @lecture2 = FactoryBot.create(:lecture)
-    @lecture2.update(status: "running")
-    visit(course_path(id: @lecture.course.id))
-    expect(page).to_not have_link("View", href: course_lecture_path(course_id: @lecture2.course.id, id: @lecture2.id))
   end
 end
