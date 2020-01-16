5d9a3a862e605d931f1d4418dea0d4c4b46494ea
Christopher Aust <38725693+caustt@users.noreply.github.com>
2020-01-16 16:37:33 +0100

    Update app/controllers/uploaded_files_controller.rb
    Co-Authored-By: Adrian Jost <22987140+adrianjost@users.noreply.github.com>

--- app/controllers/uploaded_files_controller.rb
+++ app/controllers/uploaded_files_controller.rb
@@ -64,7 +64,7 @@ class UploadedFilesController < ApplicationController
     def validate_destroy_rights
       uploaded_file = UploadedFile.find(params[:id])
       @owner = current_user == @uploaded_file.author
-      @course_file_and_course_owner = (@uploaded_file.allowsUpload.class == Course) && (@uploaded_file.allowsUpload.creator_id == current_user.id)
+      course_file_and_course_owner = (uploaded_file.allowsUpload.class == Course) && (uploaded_file.allowsUpload.creator_id == current_user.id)
       lecture_file_and_lecture_owner = (uploaded_file.allowsUpload.class == Lecture) && (uploaded_file.allowsUpload.lecturer_id == current_user.id)
       unless owner || course_file_and_course_owner || lecture_file_and_lecture_owner
         redirect_to (uploaded_files_url), notice: "You can't delete this file."
