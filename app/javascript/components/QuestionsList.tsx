cdfd0e2b816c5646b721f29775baa48a685272a8
Vincent Xeno Rahn <vinni@akv.i24.cc>
2020-01-17 16:19:17 +0100

    Adjust styling to hide space before questions list if no form is shown

--- app/javascript/components/QuestionsList.tsx
+++ app/javascript/components/QuestionsList.tsx
@@ -4,20 +4,21 @@ import Question from "./Question";
 import { QuestionsRootStoreModel } from "../stores/QuestionsRootStore";
 import {useInjectQuestions} from "../hooks/useInject";
 
-const mapStore = ({ is_student, questionsList }: QuestionsRootStoreModel) => ({
+const mapStore = ({ is_student, questionsList, interactions_enabled }: QuestionsRootStoreModel) => ({
   is_student,
-  questionsList
+  questionsList,
+  interactions_enabled
 });
 
 const QuestionsList: React.FunctionComponent<{}> = observer(() => {
-  const { is_student, questionsList } = useInjectQuestions(mapStore);
+  const { is_student, questionsList, interactions_enabled } = useInjectQuestions(mapStore);
 
   const onSortingClick = () => {
     questionsList.toggleSorting();
   };
 
   return (
-    <div className={"questionsList" + (is_student ? "" : " is_lecturer")}>
+    <div className={"questionsList" + (interactions_enabled && is_student ? " interactions_enabled" : "" )}>
       <div className="questionsFilter">
         <div className="sorting" onClick={onSortingClick}>
           {questionsList.is_sorted_by_time ? "time" : "votes"}
