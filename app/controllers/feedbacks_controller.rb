eb8b57e3da82853a9bc286a8788b17b055d6132c
Gritta Weisheit <gritta.weisheit@giz.berlin>
2020-01-17 18:29:43 +0100

    make unjoined student unable to submit feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,4 +1,5 @@
 class FeedbacksController < ApplicationController
+  before_action :validate_joined_user_or_owner
   def create
     @lecture = Lecture.find(params[:lecture_id])
     @feedback = @lecture.feedbacks.create(comment_params)
@@ -7,5 +8,12 @@ class FeedbacksController < ApplicationController
   private
     def comment_params
       params.require(:feedback).permit(:content)
+    end
+
+    def validate_joined_user_or_owner
+      isStudent = current_user.is_student
+      isJoinedStudent = isStudent && @lecture.participating_students.include?(current_user)
+      isLectureOwner = !isStudent && @lecture.lecturer == current_user
+      return head :forbidden unless isJoinedStudent || isLectureOwner
     end
 end
