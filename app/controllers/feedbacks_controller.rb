cd99185231f7536f6b698cb9a39b7d4a9b8e3d9e
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-21 18:59:58 +0100

    [#169] Add visual feedback for submitting

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,8 +3,12 @@ class FeedbacksController < ApplicationController
   def create
     @lecture = Lecture.find(params[:lecture_id])
     @feedback = @lecture.feedbacks.create(content: comment_params[:content], user: current_user)
-    head :no_content
     #redirect_to course_lecture_path(@lecture.course, @lecture)
+  end
+
+  def update
+    @lecture = Lecture.find(params[:lecture_id])
+    @feedback.update(comment_params)
   end
 
   private
