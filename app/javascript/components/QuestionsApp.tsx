ed6316519f2409154962c61642e9fe9d4028a31c
vxrahn <33007914+vxrahn@users.noreply.github.com>
2020-01-22 20:50:26 +0100

    Feature/#73 disallow question interaction in archived lecture (#207)
    * Enable question interactions only if lecture is readonly
    
    * Enable update interaction only if lecture not readonly
    
    * Adjust styling to hide space before questions list if no form is shown
    
    Co-authored-by: LoWanda <34399700+LoWanda@users.noreply.github.com>
    Co-authored-by: @SimonWiet 
    Co-authored-by: @FabianGalandi

--- app/javascript/components/QuestionsApp.tsx
+++ app/javascript/components/QuestionsApp.tsx
@@ -9,14 +9,16 @@ interface IQuestionsAppProps {
     user_id: number,
     is_student: boolean,
     lecture_id: number,
-    course_id: number
+    course_id: number,
+    interactions_enabled: boolean
 }
 
-const QuestionsApp: React.FunctionComponent<IQuestionsAppProps> = ({ user_id, is_student, lecture_id, course_id }) => {
+const QuestionsApp: React.FunctionComponent<IQuestionsAppProps> = ({ user_id, is_student, lecture_id, course_id, interactions_enabled }) => {
     rootStore.setUserId(user_id);
     rootStore.setIsStudent(is_student);
     rootStore.setLectureId(lecture_id);
     rootStore.setCourseId(course_id);
+    rootStore.setInteractionsEnabled(interactions_enabled);
     initQuestionsApp(rootStore);
     return (
         <StoreProvider value={rootStore}>
--- app/javascript/components/UpdatesApp.tsx
+++ app/javascript/components/UpdatesApp.tsx
@@ -7,14 +7,16 @@ const rootStore = createUpdatesRootStore();
 interface IUpdatesAppProps {
     user_id: number,
     is_student: boolean,
-    lecture_id: number
-    course_id: number
+    lecture_id: number,
+    course_id: number,
+    interactions_enabled: boolean
 }
 
-const UpdatesApp: React.FunctionComponent<IUpdatesAppProps> = ({ user_id, is_student, course_id, lecture_id }) => {
+const UpdatesApp: React.FunctionComponent<IUpdatesAppProps> = ({ user_id, is_student, course_id, lecture_id, interactions_enabled }) => {
     rootStore.setUserId(user_id);
     rootStore.setIsStudent(is_student);
     rootStore.setLectureId(lecture_id);
+    rootStore.setInteractionsEnabled(interactions_enabled);
     rootStore.setCourseId(course_id);
     initUpdatesApp(rootStore);
     return (
