c6178838044a0759576c0a98268529c8d438e565
marc.rosenau@student.hpi.de <marc.rosenau@student.hpi.de>
2020-01-22 15:01:53 +0100

    [#169] Remove notification disappearance

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,8 +2,14 @@ class FeedbacksController < ApplicationController
 
   def create
     @lecture = Lecture.find(params[:lecture_id])
-    @feedback = @lecture.feedbacks.create(content: comment_params[:content], user: current_user)
+    if params[:commit] == "CREATE"
+      @feedback = @lecture.feedbacks.create(content: comment_params[:content], user: current_user)
+    elsif params[:commit] == "UPDATE"
+      @feedback = @lecture.feedbacks.where(user_id: current_user.id)
+      @feedback.update(comment_params)
     #redirect_to course_lecture_path(@lecture.course, @lecture)
+    end
+    head :no_content
   end
 
   def update
