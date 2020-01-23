db621b96a58103daa99812e51059ea2a1f6f6f38
Luc Prestin <l.prestin@gmx.de>
2020-01-23 13:55:02 +0100

    make tests run and lint
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,16 +1,15 @@
 class FeedbacksController < ApplicationController
-
   def create
     @lecture = Lecture.find(params[:lecture_id])
     @feedback = @lecture.feedbacks.create(comment_params)
 
     respond_to do |format|
       if @feedback.save
-        #format.html {redirect_to @feedback, notice: 'User was successfully created.'}
+        # format.html {redirect_to @feedback, notice: 'User was successfully created.'}
         format.js
-        #format.json {render json: @feedback, status: :created, location: @feedback }
+        # format.json {render json: @feedback, status: :created, location: @feedback }
       else
-        format.html { render action: "new", notice: 'Error'}
+        format.html { render action: "new", notice: "Error" }
       end
     end
   end
