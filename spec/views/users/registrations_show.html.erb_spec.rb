89a99f134150a0d82003e00f52c90597b4388db3
LucPrestin <40767277+LucPrestin@users.noreply.github.com>
2020-01-24 15:22:24 +0100

    Feature/#175 calendar feed 3 (#227)
    * Merge branch 'dev' of /Users/tam/Documents/5. Semester/SWTII/lecture-portal with conflicts.
    
    * Add webcal url for every user for constant updates of the visited lectures slots.
    
    Add calendar support with "icalendar" gem.
    Add IcalController and "/ical" route.
    Add hash_id to every user for non guessable urls because webcal calendar feeds need to be able to be accessed without authentification.
    
    * beautify layout of the profile view
    
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * add tests
    
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * make tests run and lint
    
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * remove calendar from schema.rb
    
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    
    * add hash_id to user factory, remove save_and_open page
    
    * Remove not needed code snippets from previous merges.
    
    Co-authored-by: Theresa Hradilak <34538290+phoeinx@users.noreply.github.com>
    Co-authored-by: Jasperhino <33397387+Jasperhino@users.noreply.github.com>
    Co-authored-by: Marc  <35168157+ma-ro@users.noreply.github.com>

--- spec/views/users/registrations_show.html.erb_spec.rb
+++ spec/views/users/registrations_show.html.erb_spec.rb
@@ -22,6 +22,14 @@ RSpec.describe "users/edit", type: :view do
     login_student
     visit users_show_path
     expect(page).to have_text("Calendar Feed")
+    expect(page).to have_text(@student.hash_id)
+  end
+
+  it "renders a subscription link for the users" do
+    Capybara.app_host = "http://test.host"
+    login_student
+    visit users_show_path
+    expect(page).to have_link("Subscribe", href: ical_url(hash_id: @student.hash_id, protocol: :webcal, format: :ics))
   end
 
   def login_student
