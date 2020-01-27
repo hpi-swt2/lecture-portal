4796b0c0558dd6bdffa16d92c2b684ca8132a922
swietheger <swietheger@universum.com>
2020-01-27 20:41:00 +0100

    Fix problem in lecture activation due to timezones

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module LecturePortal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -18,6 +21,11 @@ module LecturePortal
 
     # Schedule a check for aging of comprehension stamps
     config.after_initialize do
+      ActiveRecord::Base.connection_pool.with_connection do
+        Lecture.eliminate_comprehension_stamps
+        Lecture.handle_activations
+      end
+
       Rufus::Scheduler.singleton.every '30s' do
         ActiveRecord::Base.connection_pool.with_connection do
           Lecture.eliminate_comprehension_stamps
