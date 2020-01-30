d40f6b21aa721dbef76ee57a8d0875a1986daebd
Wanda <wanda.baltzer@student.hpi.de>
2020-01-30 15:04:57 +0100

    Remove console log

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,10 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-console.log(document.URL);
 $(document).on('turbolinks:load', function() {
   if (document.URL.match("#")) {
-      console.log(document.URL);
     $('.nav-tabs a[href="#' + document.URL.split("#")[1] + '"]').tab("show");
   }
 });
