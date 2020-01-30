b87fe99f029a5a87345be952c1d8c2c55d31f315
Wanda <wanda.baltzer@student.hpi.de>
2020-01-30 15:02:23 +0100

    Redirect path when editing lectures to settings

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+console.log(document.URL);
+$(document).on('turbolinks:load', function() {
+  if (document.URL.match("#")) {
+      console.log(document.URL);
+    $('.nav-tabs a[href="#' + document.URL.split("#")[1] + '"]').tab("show");
+  }
+});
