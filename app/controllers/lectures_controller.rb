df691fe5d7335f1be0e4d073a2484ce277b8dc31
Luc Prestin <l.prestin@gmx.de>
2020-01-31 13:23:17 +0100

    remove redirect when lecture is running, fix back button link in the lecture edit page

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -38,9 +38,7 @@ class LecturesController < ApplicationController
 
   # GET courses/:course_id/lectures/1/edit
   def edit
-    if @lecture.status != "created"
-      redirect_to course_lecture_path(@course, @lecture), alert: "This page is only available before a lecture was started. Use the settings tab instead."
-    end
+
   end
 
   # POST courses/:course_id/lectures
