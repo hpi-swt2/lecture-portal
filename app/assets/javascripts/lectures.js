c7cf1c78050a082935ff35e20ee32e5f79d59662
Wanda <wanda.baltzer@student.hpi.de>
2020-01-31 13:56:08 +0100

    Refactor updating in soft realtime after status change

--- app/assets/javascripts/lectures.js
+++ app/assets/javascripts/lectures.js
@@ -1,14 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-(function() {
+function initLectureStatusUpdating(isRelevantLectureStatusUpdated) {
     App.cable.subscriptions.create(
         {channel: "LectureStatusChannel"},
         {
             received: function (data) {
-              if (typeof isRelevantLectureStatusUpdated === "function" && isRelevantLectureStatusUpdated(data.lecture_id, data.course_id)) {
+             if (isRelevantLectureStatusUpdated(data.lecture_id, data.course_id)) {
                 window.location.reload()
               }
             }
         } 
-      )
-  }.call(this));
+    )
+}
