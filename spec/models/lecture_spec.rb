18ed82063028f35806fb99c2f13b0164554d224f
fabianhe <fabian.heseding@web.de>
2020-01-31 10:06:02 +0100

    Add blocks to travel

--- spec/models/lecture_spec.rb
+++ spec/models/lecture_spec.rb
@@ -127,47 +127,51 @@ RSpec.describe Lecture, type: :model do
   it "change status from created to active when time changes" do
     @lecture.update(date: Date.tomorrow, start_time: DateTime.now + 1.day + 1.hour, end_time: DateTime.now + 1.day + 2.hours)
     expect(@lecture.status).to eq("created")
-    travel 1.day
-    Lecture.handle_activations
-    @lecture.reload
-    expect(@lecture.status).to eq("active")
+    travel 1.day do
+      Lecture.handle_activations
+      @lecture.reload
+      expect(@lecture.status).to eq("active")
+    end
   end
 
   it "change status from active to running when time changes" do
     @lecture.update(date: Date.today, start_time: DateTime.now + 1.hour, end_time: DateTime.now + 2.hours)
     expect(@lecture.status).to eq("active")
-    travel 1.hour
-    Lecture.handle_activations
-    @lecture.reload
-    expect(@lecture.status).to eq("running")
+    travel 1.hour do
+      Lecture.handle_activations
+      @lecture.reload
+      expect(@lecture.status).to eq("running")
+    end
   end
 
   it "change status from running to active when time changes" do
     @lecture.update(date: Date.today, start_time: DateTime.now, end_time: DateTime.now + 1.hours)
     expect(@lecture.status).to eq("running")
-    travel 2.hours
-    Lecture.handle_activations
-    @lecture.reload
-    expect(@lecture.status).to eq("active")
+    travel 2.hours do
+      Lecture.handle_activations
+      @lecture.reload
+      expect(@lecture.status).to eq("active")
+    end
   end
 
   it "change status from active to archived when time changes" do
     @lecture.update(date: Date.today, start_time: DateTime.now - 2.hours, end_time: DateTime.now - 1.hour)
     expect(@lecture.status).to eq("active")
-    travel 1.day
-    Lecture.handle_activations
-    @lecture.reload
-    expect(@lecture.status).to eq("archived")
+    travel 1.day do
+      Lecture.handle_activations
+      @lecture.reload
+      expect(@lecture.status).to eq("archived")
+    end
   end
 
   it "not change status once it is archived" do
-    @lecture.update(date: Date.today, start_time: DateTime.now - 1.hour, end_time: DateTime.now + 1.hour)
-    expect(@lecture.status).to_not eq("archived")
-    travel 1.day
-    Lecture.handle_activations
-    @lecture.reload
+    @lecture.update(date: Date.yesterday, start_time: DateTime.now - 1.day - 1.hour, end_time: DateTime.now - 1.day + 1.hour)
     expect(@lecture.status).to eq("archived")
-    travel_back
+    travel -1.day do
+      Lecture.handle_activations
+      @lecture.reload
+      expect(@lecture.status).to eq("archived")
+    end
     Lecture.handle_activations
     @lecture.reload
     expect(@lecture.status).to eq("archived")
