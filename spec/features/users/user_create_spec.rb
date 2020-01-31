1adf2a8fbf7979492de6268a347caaf71d229837
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-01-31 16:13:17 +0100

    Default for secret key.

--- spec/features/users/user_create_spec.rb
+++ spec/features/users/user_create_spec.rb
@@ -15,7 +15,7 @@ describe "Creating a new user", type: :feature do
 
   context "secret key is set" do
       before :each do
-        allow(ENV).to receive(:fetch).with("SECRET_KEY", nil).and_return(@real_secret_key)
+        allow(ENV).to receive(:fetch).with("SECRET_KEY", User.secret_key_default).and_return(@real_secret_key)
       end
 
       it "makes a user who knows the secret key a lecturer" do
@@ -56,7 +56,7 @@ describe "Creating a new user", type: :feature do
 
   context "secret key is not set" do
     before :each do
-      allow(ENV).to receive(:fetch).with("SECRET_KEY", nil).and_return(nil)
+      allow(ENV).to receive(:fetch).with("SECRET_KEY", User.secret_key_default).and_return(User.secret_key_default)
     end
 
     it "makes a user who knows the secret key a lecturer" do
@@ -64,13 +64,25 @@ describe "Creating a new user", type: :feature do
       find(:id, "user_email").set(@email_lecturer)
       find(:id, "user_password").set(@password)
       find(:id, "user_password_confirmation").set(@password)
-      find(:id, "user_secret_key").set(@real_secret_key)
+      find(:id, "user_secret_key").set(User.secret_key_default)
       click_on("Sign up")
       expect(User.find_by email: @email_lecturer).to_not be_nil
-      expect(User.find_by_email(@email_lecturer).is_student).to be_truthy
+      expect(User.find_by_email(@email_lecturer).is_student).to_not be_truthy
     end
 
     it "does not make a user who does not know the secret key a lecturer" do
+      visit(new_user_registration_path)
+      find(:id, "user_email").set(@email_student)
+      find(:id, "user_password").set(@password)
+      find(:id, "user_password_confirmation").set(@password)
+      find(:id, "user_secret_key").set(@wrong_secret_key)
+      click_on("Sign up")
+      user = User.find_by email: @email_student
+      expect(user).to_not be_nil
+      expect(user.is_student).to be_truthy
+      end
+
+    it "does not make a user who uses a blank secret key a lecturer" do
       visit(new_user_registration_path)
       find(:id, "user_email").set(@email_student)
       find(:id, "user_password").set(@password)
