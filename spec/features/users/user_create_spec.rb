05c0585816c91f1db181d6e840c5aa7aa3947f8b
Eric Ackermann <eric.ackermann@student.hpi.de>
2020-01-31 16:38:11 +0100

    Rubocop

--- spec/features/users/user_create_spec.rb
+++ spec/features/users/user_create_spec.rb
@@ -71,15 +71,15 @@ describe "Creating a new user", type: :feature do
     end
 
     it "does not make a user who does not know the secret key a lecturer" do
-      visit(new_user_registration_path)
-      find(:id, "user_email").set(@email_student)
-      find(:id, "user_password").set(@password)
-      find(:id, "user_password_confirmation").set(@password)
-      find(:id, "user_secret_key").set(@wrong_secret_key)
-      click_on("Sign up")
-      user = User.find_by email: @email_student
-      expect(user).to_not be_nil
-      expect(user.is_student).to be_truthy
+        visit(new_user_registration_path)
+        find(:id, "user_email").set(@email_student)
+        find(:id, "user_password").set(@password)
+        find(:id, "user_password_confirmation").set(@password)
+        find(:id, "user_secret_key").set(@wrong_secret_key)
+        click_on("Sign up")
+        user = User.find_by email: @email_student
+        expect(user).to_not be_nil
+        expect(user.is_student).to be_truthy
       end
 
     it "does not make a user who uses a blank secret key a lecturer" do
